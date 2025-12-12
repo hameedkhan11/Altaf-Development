547e7e7a1f33c6c8961a06e213289c0f366248c2
laptech88 <laptech88@gmail.com>
2025-12-12 10:09:30 +0500

    Altaf Developments 2.0 addeddd

--- next.config.js
+++ next.config.js
@@ -33,6 +33,11 @@ const nextConfig = {
         hostname: 'images.unsplash.com',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'ik.imagekit.io',
+        pathname: '/**',
+      },
     ],
   },
 };
